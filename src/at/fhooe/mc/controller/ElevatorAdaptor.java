faa4458295730ed185c40d0943122ec01d1c138f
Tamara Wachs <Naconi@gmx.at>
2014-12-18 19:05:42 +0100

    Change-Id: I986c743a6b064c143cf709ec6a334e8800bc593a

--- src/at/fhooe/mc/controller/ElevatorAdaptor.java
+++ src/at/fhooe/mc/controller/ElevatorAdaptor.java
@@ -1,17 +1,19 @@
 /**
- * 
+ *
  */
 package at.fhooe.mc.controller;
+
+import java.rmi.RemoteException;
 
 import at.fhooe.mc.test.ElevatorMock;
 
 /**
  * @author Metrics_Testing Team
  * Dec 18, 2014
- * 
+ *
  */
 public class ElevatorAdaptor implements IElevatorControls {
-	
+
 	/**
 	 * For time being referenced from mock object
 	 */
@@ -21,71 +23,71 @@ public class ElevatorAdaptor implements IElevatorControls {
 	 * @see at.fhooe.mc.controller.IElevatorControls#getCommittedDirection(int)
 	 */
 	@Override
-	public int getCommittedDirection(int elevatorNumber) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.fhooe.mc.controller.IElevatorControls#getElevatorDoorStatus(int)
-	 */
-	@Override
-	public int getElevatorDoorStatus(int elevatorNumber) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/* (non-Javadoc)
-	 * @see at.fhooe.mc.controller.IElevatorControls#getElevatorPosition(int)
-	 */
-	@Override
-	public int getElevatorPosition(int elevatorNumber) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getCommittedDirection(int elevatorNumber) throws RemoteException {
+		return iElevatorReference.getCommittedDirection(elevatorNumber);
 	}
 
 	/* (non-Javadoc)
 	 * @see at.fhooe.mc.controller.IElevatorControls#getElevatorSpeed(int)
 	 */
 	@Override
-	public int getElevatorSpeed(int elevatorNumber) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getElevatorDoorStatus(int elevatorNumber) throws RemoteException {
+		return iElevatorReference.getElevatorDoorStatus(elevatorNumber);
+	}
+
+	/* (non-Javadoc)
+	 * @see at.fhooe.mc.controller.IElevatorControls#getElevatorPosition(int)
+	 */
+	@Override
+	public int getElevatorPosition(int elevatorNumber) throws RemoteException {
+		return iElevatorReference.getElevatorPosition(elevatorNumber);
+	}
+
+	/* (non-Javadoc)
+	 * @see at.fhooe.mc.controller.IElevatorControls#getElevatorSpeed(int)
+	 */
+	@Override
+	public int getElevatorSpeed(int elevatorNumber) throws RemoteException{
+		return iElevatorReference.getElevatorSpeed(elevatorNumber);
 	}
 
 	/* (non-Javadoc)
 	 * @see at.fhooe.mc.controller.IElevatorControls#getElevatorWeight(int)
 	 */
 	@Override
-	public int getElevatorWeight(int elevatorNumber) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getElevatorWeight(int elevatorNumber) throws RemoteException {
+		return iElevatorReference.getElevatorWeight(elevatorNumber);
 	}
 
 	/* (non-Javadoc)
 	 * @see at.fhooe.mc.controller.IElevatorControls#getElevatorCapacity(int)
 	 */
 	@Override
-	public int getElevatorCapacity(int elevatorNumber) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getElevatorCapacity(int elevatorNumber) throws RemoteException{
+		return iElevatorReference.getElevatorCapacity(elevatorNumber);
+	}
+
+	/* (non-Javadoc)
+	 * @see at.fhooe.mc.controller.IElevatorControls#setCommittedDirection(int, int)
+	 */
+	public int getFloorNum() throws java.rmi.RemoteException {
+		return iElevatorReference.getFloorNum();
 	}
 
 	/* (non-Javadoc)
 	 * @see at.fhooe.mc.controller.IElevatorControls#setCommittedDirection(int, int)
 	 */
 	@Override
-	public void setCommittedDirection(int elevatorNumber, int direction) {
-		// TODO Auto-generated method stub
-
+	public void setCommittedDirection(int elevatorNumber, int direction) throws RemoteException{
+		iElevatorReference.setCommittedDirection(elevatorNumber, direction);
 	}
 
 	/* (non-Javadoc)
 	 * @see at.fhooe.mc.controller.IElevatorControls#setServicesFloors(int, int, boolean)
 	 */
 	@Override
-	public void setServicesFloors(int elevatorNumber, int floor, boolean service) {
-		// TODO Auto-generated method stub
+	public void setServicesFloors(int elevatorNumber, int floor, boolean service) throws RemoteException {
+		iElevatorReference.setServicesFloors(elevatorNumber, floor, service);
 
 	}
 
@@ -93,36 +95,53 @@ public class ElevatorAdaptor implements IElevatorControls {
 	 * @see at.fhooe.mc.controller.IElevatorControls#setTarget(int, int)
 	 */
 	@Override
-	public void setTarget(int elevatorNumber, int target) {
-		// TODO Auto-generated method stub
-
+	public void setTarget(int elevatorNumber, int target) throws RemoteException{
+		iElevatorReference.setTarget(elevatorNumber, target);
 	}
 
 	/* (non-Javadoc)
 	 * @see at.fhooe.mc.controller.IElevatorControls#getPressedButtonsElevator(int)
 	 */
 	@Override
-	public boolean[] getPressedButtonsElevator(int elevatorNumber) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean[] getPressedButtonsElevator(int elevatorNumber) throws RemoteException{
+		int floorNum = getFloorNum();
+		boolean buttonsSet[] = new boolean[floorNum];
+
+		for (int i = 0; i < floorNum; i++){
+			buttonsSet[i] = iElevatorReference.getElevatorButton(elevatorNumber, i+1);
+		}
+
+		return buttonsSet;
 	}
 
 	/* (non-Javadoc)
 	 * @see at.fhooe.mc.controller.IElevatorControls#getPressedButtonsFloorDown()
 	 */
 	@Override
-	public boolean[] getPressedButtonsFloorDown() {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean[] getPressedButtonsFloorDown() throws RemoteException {
+		int floorNum = getFloorNum();
+		boolean buttonsSet[] = new boolean[floorNum];
+
+		for (int i = 0; i < floorNum; i++){
+			buttonsSet[i] = iElevatorReference.getFloorButtonDown(i+1);
+		}
+
+		return buttonsSet;
 	}
 
 	/* (non-Javadoc)
 	 * @see at.fhooe.mc.controller.IElevatorControls#getPressedButtonsFloorUp()
 	 */
 	@Override
-	public boolean[] getPressedButtonsFloorUp() {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean[] getPressedButtonsFloorUp() throws RemoteException {
+		int floorNum = getFloorNum();
+		boolean buttonsSet[] = new boolean[floorNum];
+
+		for (int i = 0; i < floorNum; i++){
+			buttonsSet[i] = iElevatorReference.getFloorButtonUp(i+1);
+		}
+
+		return buttonsSet;
 	}
 
 	/**
--- src/at/fhooe/mc/controller/IElevatorControls.java
+++ src/at/fhooe/mc/controller/IElevatorControls.java
@@ -1,107 +1,126 @@
 /**
- * 
+ *
  */
 package at.fhooe.mc.controller;
+
+import java.rmi.RemoteException;
 
 /**
  * @author Metrics_Testing Team
  * Dec 18, 2014
- * 
+ *
  */
 public interface IElevatorControls {
-	
+
 	/**
-	 * Retrieves the committed direction of the specified elevator (up / down / uncommitted). 
-	 * @param elevatorNumber - elevator number whose committed direction is being retrieved 
+	 * Retrieves the committed direction of the specified elevator (up / down / uncommitted).
+	 * @param elevatorNumber - elevator number whose committed direction is being retrieved
 	 * @return the current direction of the specified elevator where up=0, down=1 and uncommitted=2
+	 * @throws java.rmi.RemoteException
 	 */
-	public int getCommittedDirection(int elevatorNumber); 
-	
+	public int getCommittedDirection(int elevatorNumber) throws RemoteException ;
+
 	/**
-	 * Provides the current status of the doors of the specified elevator (open/closed).      
-	 * @param elevatorNumber - elevator number whose door status is being retrieved 
+	 * Provides the current status of the doors of the specified elevator (open/closed).
+	 * @param elevatorNumber - elevator number whose door status is being retrieved
 	 * @return returns the door status of the indicated elevator where 1=open and 2=closed
+	 * @throws RemoteException
 	 */
-	public int getElevatorDoorStatus(int elevatorNumber); 
-	
-	
+	public int getElevatorDoorStatus(int elevatorNumber)  throws RemoteException;
+
+
 	/**
-	 * Provides the current location of the specified elevator in feet from the bottom of the building. 
-	 * @param elevatorNumber  - elevator number whose location is being retrieved 
+	 * Provides the current location of the specified elevator in feet from the bottom of the building.
+	 * @param elevatorNumber  - elevator number whose location is being retrieved
 	 * @return returns the location in feet of the indicated elevator from the bottom of the building
+	 * @throws RemoteException
 	 */
-	public int getElevatorPosition(int elevatorNumber); 
-	
-	
+	public int getElevatorPosition(int elevatorNumber) throws RemoteException;
+
+
 	/**
-	 * Provides the current speed of the specified elevator in feet per sec. 
-	 * @param elevatorNumber - elevator number whose speed is being retrieved 
+	 * Provides the current speed of the specified elevator in feet per sec.
+	 * @param elevatorNumber - elevator number whose speed is being retrieved
 	 * @return returns the speed of the indicated elevator where positive speed is up and negative is down
+	 * @throws RemoteException
 	 */
-	public int getElevatorSpeed(int elevatorNumber);
-	
-	
+	public int getElevatorSpeed(int elevatorNumber) throws RemoteException;
+
+
 	/**
-	 * Retrieves the weight of passengers on the specified elevator. 
+	 * Retrieves the weight of passengers on the specified elevator.
 	 * @param elevatorNumber - elevator number whose service is being retrieved
 	 * @return total weight of all passengers in lbs
+	 * @throws RemoteException
 	 */
-	public int getElevatorWeight(int elevatorNumber); 
-	
-	
+	public int getElevatorWeight(int elevatorNumber) throws RemoteException;
+
+
 	/**
 	 * Retrieves the maximum number of passengers that can fit on the specified elevator.
 	 * @param elevatorNumber - elevator number whose service is being retrieved
 	 * @return number of passengers
+	 * @throws RemoteException
 	 */
-	public int getElevatorCapacity(int elevatorNumber);
-	
-	
+	public int getElevatorCapacity(int elevatorNumber) throws RemoteException;
+
 	/**
-	 * Sets the committed direction of the specified elevator (up / down / uncommitted). 
+	 * Retrieves the number of floors in the building.
+	 * @return total number of floors
+	 */
+	public int getFloorNum() throws java.rmi.RemoteException;
+
+	/**
+	 * Sets the committed direction of the specified elevator (up / down / uncommitted).
 	 * @param elevatorNumber elevator number whose committed direction is being set
 	 * @param direction direction being set where up=0, down=1 and uncommitted=2
+	 * @throws RemoteException
 	 */
-	public void setCommittedDirection(int elevatorNumber, int direction);
+	public void setCommittedDirection(int elevatorNumber, int direction) throws RemoteException;
 
-	
+
 	/**
-	 * Sets whether or not the specified elevator will service the specified floor (yes/no). 
+	 * Sets whether or not the specified elevator will service the specified floor (yes/no).
 	 * @param elevatorNumber elevator number whose service is being defined
 	 * @param floor floor whose service by the specified elevator is being set
 	 * @param service indicates whether the floor is serviced by the specified elevator (yes=true,no=false)
+	 * @throws RemoteException
 	 */
-	public void setServicesFloors(int elevatorNumber, int floor, boolean service); 
-	
-	
-	/**
-	 * Sets the floor target of the specified elevator. 
-	 * @param elevatorNumber elevator number whose target floor is being set
-	 * @param target floor number which the specified elevator is to target
-	 */
-	public void setTarget(int elevatorNumber, int target); 
+	public void setServicesFloors(int elevatorNumber, int floor, boolean service) throws RemoteException;
+
 
 	/**
-	 * Provides the status of a floor request button on a specified elevator (on/off).      
+	 * Sets the floor target of the specified elevator.
+	 * @param elevatorNumber elevator number whose target floor is being set
+	 * @param target floor number which the specified elevator is to target
+	 * @throws RemoteException
+	 */
+	public void setTarget(int elevatorNumber, int target) throws RemoteException;
+
+	/**
+	 * Provides the status of a floor request button on a specified elevator (on/off).
 	 * @param elevatorNumber - elevator number whose button status is being retrieved
  	 * @return returns boolean array indicating which buttons are pressed inside this elevator
  	 * 			(example true,false,false ...  ) indicating (1 is pressed, 2 is not pressed, 2 is not pressed and so on)
+ 	 * @throws RemoteException
 	 */
-	public boolean[] getPressedButtonsElevator(int elevatorNumber);
-	
+	public boolean[] getPressedButtonsElevator(int elevatorNumber) throws RemoteException;
+
 	/**
-	 * Provides the status of the Down button on all the floor (on/off). 
+	 * Provides the status of the Down button on all the floor (on/off).
 	 * @return returns boolean array to indicate if DOWN button is active (true) or not (false)
 	 * 			for all the floors in building
+	 * @throws RemoteException
 	 */
-	public boolean[] getPressedButtonsFloorDown();
-	
+	public boolean[] getPressedButtonsFloorDown() throws RemoteException;
+
 	/**
-	 * Provides the status of the Up button on all the floors (on/off). 
+	 * Provides the status of the Up button on all the floors (on/off).
  	 * @return returns boolean array to indicate if UP button is active (true) or not (false)
 	 * 			for all the floors in building
+	 * @throws RemoteException
 	 */
-	public boolean[] getPressedButtonsFloorUp();
-	
-	
+	public boolean[] getPressedButtonsFloorUp() throws RemoteException;
+
+
 }
